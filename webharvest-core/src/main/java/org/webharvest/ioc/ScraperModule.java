6417acb7fbb0b8426f8f6bbc8dd6b81aeccebb38
pdyraga <pdyraga@a57e4815-81e3-4922-91ae-0d9c2f5eb974>
2012-10-10 14:20:32 +0000

    Technical commit: Modified ScraperModule by changing the way how event bus is instantiated and how event listeners subscribes. Previous solution caused construction of multiple event bus instances.
    git-svn-id: https://svn.code.sf.net/p/web-harvest/code/branches/release-2.1@517 a57e4815-81e3-4922-91ae-0d9c2f5eb974

--- webharvest-core/src/main/java/org/webharvest/ioc/ScraperModule.java
+++ webharvest-core/src/main/java/org/webharvest/ioc/ScraperModule.java
@@ -1,8 +1,17 @@
 package org.webharvest.ioc;
 
+import org.webharvest.runtime.database.ConnectionFactory;
+import org.webharvest.runtime.database.StandaloneConnectionPool;
+
+import com.google.common.eventbus.EventBus;
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.matcher.Matchers;
+import com.google.inject.spi.InjectionListener;
+import com.google.inject.spi.TypeListener;
+import com.google.inject.spi.TypeEncounter;;
 
 // TODO Add javadoc
 // TODO Add unit test
@@ -15,8 +24,25 @@ public final class ScraperModule extends AbstractModule {
     protected void configure() {
         install(new FactoryModuleBuilder().
                 build(ScraperFactory.class));
+        bind(ConnectionFactory.class).to(StandaloneConnectionPool.class).in(Singleton.class); //FIXME: do we need custom scope (Scraper's lifetime scope)?
+
         // FIXME rbala EventBusTypeListener will create instance of event bus
-        bindListener(Matchers.any(), new EventBusTypeListener());
+        //bindListener(Matchers.any(), new EventBusTypeListener());
+
+        final EventBus eventBus = new EventBus();
+        bind(EventBus.class).toInstance(eventBus);
+        bindListener(Matchers.any(), new TypeListener() {
+               @Override
+               public <I> void hear(@SuppressWarnings("unused") final TypeLiteral<I> typeLiteral, final TypeEncounter<I> typeEncounter) {
+                   typeEncounter.register(new InjectionListener<I>() {
+                       @Override public void afterInjection(final I instance) {
+                           eventBus.register(instance);
+                       }
+                   });
+               }
+            });
+
+
     }
 
 }
