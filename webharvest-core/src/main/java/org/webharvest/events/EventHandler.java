b5608967f5ca30da962811d197419450deb4b820
rbala <rbala@a57e4815-81e3-4922-91ae-0d9c2f5eb974>
2012-10-23 20:53:09 +0000

    Feature Request #24 Introduced event based communication helper interfaces EventHandler (allows to create old style even handlers - based on listeners), EventSink (wrapper for various event bus implementations), HarvesterEvent (represents inter Harvester's communication events), HandlerHolder (storage for event's handlers). Added HarvesterEventSink capable to route published event to appropriate Harvester.
    git-svn-id: https://svn.code.sf.net/p/web-harvest/code/branches/release-2.1@565 a57e4815-81e3-4922-91ae-0d9c2f5eb974

--- webharvest-core/src/main/java/org/webharvest/events/EventHandler.java
+++ webharvest-core/src/main/java/org/webharvest/events/EventHandler.java
@@ -1,10 +1,22 @@
 package org.webharvest.events;
 
-// TODO Missing documentation
+/**
+ * Represents an object that is handler for particular type of event.
+ *
+ * @author Robert Bala
+ * @since 2.1.0-SNAPSHOT
+ * @version %I%, %G%
+ *
+ * @param <T> Handler's supported type of event.
+ */
 public interface EventHandler<T> {
 
+    /**
+     * Handle event of supported type..
+     *
+     * @param event event to handle.
+     */
     // FIXME rbala Implement solution that does not require to declare @Subscribe annotiation
-    // TODO Missing documentation
     void handle(T event);
 
 }
--- webharvest-core/src/main/java/org/webharvest/events/HandlerHolder.java
+++ webharvest-core/src/main/java/org/webharvest/events/HandlerHolder.java
@@ -1,13 +1,23 @@
 package org.webharvest.events;
 
-//TODO Missing documentation
+/**
+ * Represents an object that serves purpose as storage of {@link EventHandler}
+ * (supporting different types of events).
+ * Handlers can be registered but not unregistered.
+ *
+ * @author Robert Bala
+ * @since 2.1.0-SNAPSHOT
+ * @version %I%, %G%
+ * @see EventHandler
+ */
 public interface HandlerHolder {
 
-    // TODO Missing documentation
+    /**
+     * Register event handler.
+     *
+     * @param handler event handler to register.
+     */
     // FIXME rbala Do we need a registration handler with unregister method?
     void register(EventHandler<?> handler);
-
-    // TODO Missing documentation
-    void subscribe();
 
 }
