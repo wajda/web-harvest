3a6ba7ecce1bc343b117d0732700031b00a562e5
rbala <rbala@a57e4815-81e3-4922-91ae-0d9c2f5eb974>
2012-10-30 14:09:51 +0000

    Feature request #24 Added missing documentation and unit tests for ScrapingInterceptor and ScrapingAwareHelper classes.
    git-svn-id: https://svn.code.sf.net/p/web-harvest/code/branches/release-2.1@583 a57e4815-81e3-4922-91ae-0d9c2f5eb974

--- webharvest-core/src/main/java/org/webharvest/ioc/ScrapingInterceptor.java
+++ webharvest-core/src/main/java/org/webharvest/ioc/ScrapingInterceptor.java
@@ -1,6 +1,5 @@
 package org.webharvest.ioc;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +13,22 @@ import org.webharvest.ScrapingAware;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
 
-// TODO Missing documentation
-// TODO Missing unit test
+/**
+ * Guice AOP interceptor responsible for taking action for method annotated with
+ * {@link Scraping} annotation. New scraping scope is created for each annotated
+ * method and is persistent until the end of invocation. Only ancestors of
+ * {@link Harvester} are applicable. An attempt to apply scraping scope to any
+ * other type will end up with thrown {@link IllegalStateException}. Just after
+ * the scope is created and shortly before it is left all registered
+ * {@link ScrapingAware} listeners are notified about the lifecycle change.
+ *
+ * @author Robert Bala
+ * @since 2.1.0-SNAPSHOT
+ * @version %I%, %G%
+ * @see Scraping
+ * @see MethodInterceptor
+ * @see ScrapingAware
+ */
 public final class ScrapingInterceptor implements MethodInterceptor {
 
     private static final Logger LOG = LoggerFactory
@@ -26,23 +39,33 @@ public final class ScrapingInterceptor implements MethodInterceptor {
      */
     @Override
     public Object invoke(final MethodInvocation invocation) throws Throwable {
-        if (!(invocation.getThis() instanceof Harvester)) {
+        final Object subject = invocation.getThis();
+        if (!(subject instanceof Harvester)) {
             throw new IllegalStateException("Not an instance of Harvester");
         }
         return invoke(InjectorHelper.getInjector(),
-                (Harvester) invocation.getThis(), new Callback() {
+                (Harvester) subject, new Callback() {
 
-            @Override
-            public Object execute() throws Throwable {
-                return invocation.proceed();
-            }});
+                    @Override
+                    public Object execute() throws Throwable {
+                        return invocation.proceed();
+                    }
+                });
     }
 
+    /**
+     * Helper method that creates new scraping scope, executes one of the
+     * {@link Harvester} object's method (the one annotated with
+     * {@code Scraping} annotation) and leaves it. Just after the scope is
+     * created and shortly before it is left all registered
+     * {@link ScrapingAware} listeners are notified about the lifecycle change.
+     */
+    // FIXME rbala Since we use injector directly then we can not test scope management on unit level!
     private Object invoke(final Injector injector, final Harvester harvester,
             final Callback callback) throws Throwable {
         final ScraperScope scope = injector.getInstance(ScraperScope.class);
-        final ScrapingAwareHelper helper =
-                injector.getInstance(ScrapingAwareHelper.class);
+        final ScrapingAwareHelper helper = injector
+                .getInstance(ScrapingAwareHelper.class);
         scope.enter(injector.getInstance(AttributeHolder.class));
         helper.onBeforeScraping(harvester);
         try {
@@ -53,31 +76,79 @@ public final class ScrapingInterceptor implements MethodInterceptor {
         }
     }
 
-    // TODO Missing documentation
+    /**
+     * Represents method invocation helper that allows to delay execution of
+     * code to the last responsible moment and treat it just like any regular
+     * parameter to any method.
+     *
+     * @author Robert Bala
+     * @since 2.1.0-SNAPSHOT
+     * @version %I%, %G%
+     */
     interface Callback {
 
-        // TODO Missing documentation
+        /**
+         * Execute some code in delayed fashion.
+         *
+         * @return result of reflective method invocation.
+         * @throws Throwable
+         *             Occurred exception thrown upon execution.
+         */
         Object execute() throws Throwable;
 
     }
 
-    // TODO Missing documentation
+    /**
+     * Guice aware helper class that maintains collection of registered
+     * {@link ScrapingAware} listeners. It simplifies notification of all
+     * registered listeners about updates the instances of {@link ScrapingAware}
+     * are interested in. That is when new scraping scope is created or tore
+     * down. It has almost exactly the same methods (
+     * {@link #onBeforeScraping(Harvester)} and
+     * {@link #onAfterScraping(Harvester)} as those introduced by
+     * {@link ScrapingAware}. However by contrast it does not implement this
+     * interface as there is a threat it would be automatically self-registered
+     * by Guice. The instance of this object is meant to be a singleton
+     * (especially for Guice).
+     *
+     * @author Robert Bala
+     * @since 2.1.0-SNAPSHOT
+     * @version %I%, %G%
+     * @see ScrapingAware
+     */
     // TODO Missing unit test
+    // TODO It is easy to register listener but how about unregistering it?
+    // TODO Resolve duplication of methods with ScrapingAware
     @Singleton
     public static final class ScrapingAwareHelper {
 
+        /**
+         * Collection of {@link ScrapingAware} listeners.
+         */
         private final List<ScrapingAware> listeners =
                 new ArrayList<ScrapingAware>();
 
-        // TODO Missing documentation
-        // TODO Missing unit test
+        /**
+         * Register {@link ScrapingAware} listener.
+         *
+         * @param listener
+         *            reference to listener object.
+         */
         // FIXME rbala Not thread safe
         // TODO Synchronize listener's registration
         public void addListener(final ScrapingAware listener) {
+            if (listener == null) {
+                throw new IllegalArgumentException("Listener is required");
+            }
             listeners.add(listener);
         }
 
-        // TODO Missing documentation
+        /**
+         * Notify all registered listeners about new scraping scope.
+         *
+         * @param harvester
+         *            reference to the {@link Harvester} the event applies to.
+         */
         // TODO Missing unit test
         public void onBeforeScraping(final Harvester harvester) {
             LOG.debug("Entering scraper scope [{}]", harvester);
@@ -86,7 +157,13 @@ public final class ScrapingInterceptor implements MethodInterceptor {
             }
         }
 
-        // TODO Missing documentation
+        /**
+         * Notify all registered listeners about scraping scope that is about
+         * the end.
+         *
+         * @param harvester
+         *            reference to the {@link Harvester} the event applies to.
+         */
         // TODO Missing unit test
         public void onAfterScraping(final Harvester harvester) {
             LOG.debug("Leaving scraper scope [{}]", harvester);
