dab1d1aa49bd1c3893f63f8309899b5ce3d074bc
rbala <rbala@a57e4815-81e3-4922-91ae-0d9c2f5eb974>
2012-10-31 09:00:37 +0000

    Feature request #29 Added missing documentation and unit tests for InjectorHelper class.
    git-svn-id: https://svn.code.sf.net/p/web-harvest/code/branches/release-2.1@584 a57e4815-81e3-4922-91ae-0d9c2f5eb974

--- webharvest-core/src/main/java/org/webharvest/ioc/InjectorHelper.java
+++ webharvest-core/src/main/java/org/webharvest/ioc/InjectorHelper.java
@@ -3,20 +3,48 @@ package org.webharvest.ioc;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 
-// TODO Missing documentation
-// TODO Missing unit test
+/**
+ * Guice static injector helper. This class is handy when it comes to get
+ * reference to Guice's {@link Injector} for a class that is not managed or
+ * instantiated by Guice (eg. legacy code or Guice module type listener
+ * classes). Please do not overuse it as its a kind of trick rather then good
+ * approach to codding style.
+ *
+ * <pre>
+ * public class MyModule extends AbstractModule {
+ *   protected void configure() {
+ *     // (...)
+ *     requestStaticInjection(InjectorHelper.class);
+ *     // (...)
+ *   }
+ * }
+ * </pre>
+ *
+ * @author Robert Bala
+ * @author Piotr Dyraga
+ * @since 2.1.0-SNAPSHOT
+ * @see Injector
+ */
 public final class InjectorHelper {
 
-     @Inject private static Injector injector;
+    @Inject
+    private static Injector injector;
 
-     private InjectorHelper() {
-         // DO nothing constructor
-     }
+    /**
+     * Class do nothing constructor preventing instantiation.
+     */
+    private InjectorHelper() {
+        // Do nothing constructor
+    }
 
-     // TODO Missing documentation
-     // TODO Missing unit test
-     public static Injector getInjector() {
-         return injector;
-     }
+    /**
+     * Gets reference to the current {@link Injector}.
+     *
+     * @return reference to Guice's or {@code null} if the helper has not been
+     *         installed properly.
+     */
+    public static Injector getInjector() {
+        return injector;
+    }
 
 }
