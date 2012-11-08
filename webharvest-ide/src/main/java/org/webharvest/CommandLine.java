a9234ebdc8a6eba69b0224ae709f656b949ad360
rbala <rbala@a57e4815-81e3-4922-91ae-0d9c2f5eb974>
2012-11-08 10:42:02 +0000

    Feature Request #29 Technical commit. Replaced direct instantiation of ConfigSource Location classes in CommandLine with Guice's obtained instance of factory class.
    git-svn-id: https://svn.code.sf.net/p/web-harvest/code/branches/release-2.1@624 a57e4815-81e3-4922-91ae-0d9c2f5eb974

--- webharvest-ide/src/main/java/org/webharvest/CommandLine.java
+++ webharvest-ide/src/main/java/org/webharvest/CommandLine.java
@@ -52,10 +52,10 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 import org.apache.log4j.PropertyConfigurator;
+import org.webharvest.definition.ConfigSource;
+import org.webharvest.definition.ConfigSourceFactory;
 import org.webharvest.definition.DefinitionResolver;
-import org.webharvest.definition.FileConfigSource;
 import org.webharvest.definition.IElementDef;
-import org.webharvest.definition.URLConfigSource;
 import org.webharvest.exception.PluginException;
 import org.webharvest.gui.Ide;
 import org.webharvest.ioc.DebugFileLogger;
@@ -161,13 +161,14 @@ public class CommandLine {
                 }
             };
 
-            // FIXME rbala although temporary solution it is duplicated (CommandLine)
-            final Harvest harvest = injector .getInstance(Harvest.class);
-
+            final ConfigSourceFactory configSourceFactory =
+                    injector.getInstance(ConfigSourceFactory.class);
+            final Harvest harvest = injector.getInstance(Harvest.class);
+            final ConfigSource configSource = (configLowercase.startsWith("http://") || configLowercase.startsWith("https://"))
+                    ? configSourceFactory.create(new URL(configFilePath))
+                    : configSourceFactory.create(new File(configFilePath));
             // FIXME rbala although temporary solution it is duplicated (ConfigPanel)
-            final Harvester harvester = (configLowercase.startsWith("http://") || configLowercase.startsWith("https://"))
-                    ? harvest.getHarvester(new URLConfigSource(new URL(configFilePath)), callback)
-                    : harvest.getHarvester(new FileConfigSource(new File(configFilePath)), callback);
+            final Harvester harvester = harvest.getHarvester(configSource, callback);
 
             harvester.execute(new Harvester.ContextInitCallback() {
                 @Override
