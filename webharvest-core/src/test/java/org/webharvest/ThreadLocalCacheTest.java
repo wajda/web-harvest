aac02c2259116244baf57eacc9a18f78cf22ac34
pdyraga <pdyraga@a57e4815-81e3-4922-91ae-0d9c2f5eb974>
2012-11-14 08:00:23 +0000

    Bug #56 Moved multithreaded test for ThreadLocalCache into separate class (ThreadLocalCacheMTest). Before, when it was declared in the inner class, it was not picked up by the maven test runner (it is a default setting of surefire plugin, to skip inner classes).
    git-svn-id: https://svn.code.sf.net/p/web-harvest/code/branches/release-2.1@628 a57e4815-81e3-4922-91ae-0d9c2f5eb974

--- webharvest-core/src/test/java/org/webharvest/ThreadLocalCacheTest.java
+++ webharvest-core/src/test/java/org/webharvest/ThreadLocalCacheTest.java
@@ -5,7 +5,6 @@ import static org.testng.AssertJUnit.assertNull;
 import static org.testng.AssertJUnit.assertSame;
 import static org.testng.AssertJUnit.assertTrue;
 
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.unitils.UnitilsTestNG;
@@ -53,31 +52,5 @@ public class ThreadLocalCacheTest extends UnitilsTestNG {
         cache.invalidate("mykey");
         assertNull("Object should not be available after its invalidation",
                 cache.lookup("mykey"));
-    }
-
-    /**
-     * Multithreaded test is declared inside inner class to overcome
-     * unitils's 'before method' interceptor problems.
-     * (We have to use 'before class' interceptor for multithreaded tests,
-     * otherwise we can get unexpected results, since the cache reference may
-     * change between test method invocations)
-     */
-    public static class MultithreadedTest {
-        private ThreadLocalCache<String, Object> cache;
-
-        @BeforeClass
-        public void setUp() {
-            this.cache = new ThreadLocalCache<String, Object>();
-        }
-
-        @Test(invocationCount = 100, threadPoolSize = 10)
-        public void cacheWorksLocallyPerThread() throws Exception {
-            final Long cachedObject = new Long(Thread.currentThread().getId());
-            cache.put("mykey", cachedObject);
-            assertTrue("Cache should contain previously stored object",
-                    cache.contains("mykey"));
-            assertSame("Cache should be able to lookup previously stored object",
-                    cachedObject, cache.lookup("mykey"));
-        }
     }
 }
